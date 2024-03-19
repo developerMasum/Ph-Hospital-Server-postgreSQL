b7f65420ae88a727ef625a7f33e55798125a4e84
developerMasum <developer.masum2001@gmail.com>
2024-03-19 13:19:33 +0600

    added findUnique

--- src/app/modules/Admin/admin.controller.ts
+++ src/app/modules/Admin/admin.controller.ts
@@ -1,33 +1,45 @@
-import { Request, Response } from 'express';
-import { AdminService } from './admin.service';
-import pick from '../../../shared/pick';
-import { adminFilterableFields } from './admin.constans';
-
-
-
+import { Request, Response } from "express";
+import { AdminService } from "./admin.service";
+import pick from "../../../shared/pick";
+import { adminFilterableFields } from "./admin.constans";
 
 const getAllFromDB = async (req: Request, res: Response) => {
-    try {
-        // console.log(req.query)
-        const filters = pick(req.query, adminFilterableFields);
-        const options = pick(req.query, ['limit', 'page', 'sortBy', 'sortOrder'])
-        console.log(options)
-        const result = await AdminService.getAllFromDB(filters, options)
-        res.status(200).json({
-            success: true,
-            message: "Admin data fetched!",
-            data: result
-        })
-    }
-    catch (err) {
-        res.status(500).json({
-            success: false,
-            message: err?.name || "Something went wrong",
-            error: err
-        })
-    }
-}
+  try {
+    // console.log(req.query)
+    const filters = pick(req.query, adminFilterableFields);
+    const options = pick(req.query, ["limit", "page", "sortBy", "sortOrder"]);
+    console.log(options);
+    const result = await AdminService.getAllFromDB(filters, options);
+    res.status(200).json({
+      success: true,
+      message: "Admin data fetched!",
+      meta: result.meta,
+      data: result.data,
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: err?.name || "Something went wrong",
+      error: err,
+    });
+  }
+};
+
+const getByIdFromDB = async (req: Request, res: Response) => {
+  const {id} = req.params;
+  try {
+    const result = await AdminService.getByIdFromDB(id);
+    res.status(200).json({
+      success: true,
+      message: "Admin data fetched!",
+      result,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 export const AdminController = {
-    getAllFromDB
-}
+  getAllFromDB,
+  getByIdFromDB,
+};
--- src/app/modules/Admin/admin.service.ts
+++ src/app/modules/Admin/admin.service.ts
@@ -2,51 +2,74 @@ import { Prisma } from "@prisma/client";
 import { paginationHelper } from "../../../helpars/paginationHelpers";
 import prisma from "../../../shared/prisma";
 
-
-
 const getAllFromDB = async (params: any, options: any) => {
-    const { page, limit, skip } = paginationHelper.calculatePagination(options);
-    const { searchTerm, ...filterData } = params;
-    const andConditions: Prisma.AdminWhereInput[] = [];
+  const { page, limit, skip } = paginationHelper.calculatePagination(options);
+  const { searchTerm, ...filterData } = params;
+  const andConditions: Prisma.AdminWhereInput[] = [];
 
-    //console.log(filterData);
-    if (params.searchTerm) {
-        andConditions.push({
-            OR: adminSearchAbleFields.map(field => ({
-                [field]: {
-                    contains: params.searchTerm,
-                    mode: 'insensitive'
-                }
-            }))
-        })
-    };
-
-    if (Object.keys(filterData).length > 0) {
-        andConditions.push({
-            AND: Object.keys(filterData).map(key => ({
-                [key]: {
-                    equals: filterData[key]
-                }
-            }))
-        })
-    }
-
-    //console.dir(andCondions, { depth: 'inifinity' })
-    const whereConditions: Prisma.AdminWhereInput = { AND: andConditions }
-
-    const result = await prisma.admin.findMany({
-        where: whereConditions,
-        skip,
-        take: limit,
-        orderBy: options.sortBy && options.sortOrder ? {
-            [options.sortBy]: options.sortOrder
-        } : {
-            createdAt: 'desc'
-        }
+  //console.log(filterData);
+  if (params.searchTerm) {
+    andConditions.push({
+      OR: adminSearchAbleFields.map((field) => ({
+        [field]: {
+          contains: params.searchTerm,
+          mode: "insensitive",
+        },
+      })),
     });
-    return result;
+  }
+
+  if (Object.keys(filterData).length > 0) {
+    andConditions.push({
+      AND: Object.keys(filterData).map((key) => ({
+        [key]: {
+          equals: filterData[key],
+        },
+      })),
+    });
+  }
+
+  //console.dir(andCondions, { depth: 'inifinity' })
+  const whereConditions: Prisma.AdminWhereInput = { AND: andConditions };
+
+  const result = await prisma.admin.findMany({
+    where: whereConditions,
+    skip,
+    take: limit,
+    orderBy:
+      options.sortBy && options.sortOrder
+        ? {
+            [options.sortBy]: options.sortOrder,
+          }
+        : {
+            createdAt: "desc",
+          },
+  });
+  const total = await prisma.admin.count({
+    where: whereConditions,
+  });
+  return {
+    meta: {
+      page,
+      limit,
+      total,
+    },
+    data: result,
+  };
+};
+
+const getByIdFromDB = async (id:string) => {
+  const result = await prisma.admin.findUnique({
+    where:{
+      id,
+      isDeleted:false
+    }
+  })
+  return result
+
 }
 
 export const AdminService = {
-    getAllFromDB
-}
+  getAllFromDB,
+  getByIdFromDB
+};
