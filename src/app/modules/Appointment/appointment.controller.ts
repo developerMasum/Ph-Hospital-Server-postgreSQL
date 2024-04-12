4e7140635ccd3bf734aced4231d6c0e6411d6599
developerMasum <developer.masum2001@gmail.com>
2024-04-12 12:09:49 +0600

    APPOINTMENT MODULE  completed

--- src/app/modules/Appointment/appointment.controller.ts
+++ src/app/modules/Appointment/appointment.controller.ts
@@ -4,6 +4,8 @@ import sendResponse from "../../../shared/sendResponse";
 import httpStatus from "http-status";
 import { appointmentService } from "./appointment.service";
 import { IAuthUser } from "../../interfaces/common";
+import pick from "../../../shared/pick";
+import { appointmentFilterableFields } from "./appointment.constant";
 
 const createAppointment = catchAsync(async (req: Request & { user?: IAuthUser }, res: Response) => {
     const user = req.user as IAuthUser;
@@ -17,6 +19,36 @@ const createAppointment = catchAsync(async (req: Request & { user?: IAuthUser },
     });
 });
 
+const getMyAppointment = catchAsync(async (req: Request & { user?: IAuthUser }, res: Response) => {
+    const user = req.user;
+    const filters = pick(req.query, ['status', 'paymentStatus']);
+    const options = pick(req.query, ['limit', 'page', 'sortBy', 'sortOrder']);
+
+    const result = await appointmentService.getMyAppointment(user as IAuthUser, filters, options);
+
+    sendResponse(res, {
+        statusCode: httpStatus.OK,
+        success: true,
+        message: 'My Appointment retrieve successfully',
+        data: result
+    });
+});
+const getAllFromDB = catchAsync(async (req: Request, res: Response) => {
+    const filters = pick(req.query, appointmentFilterableFields)
+    const options = pick(req.query, ['limit', 'page', 'sortBy', 'sortOrder']);
+    const result = await appointmentService.getAllFromDB(filters, options);
+    sendResponse(res, {
+        statusCode: httpStatus.OK,
+        success: true,
+        message: 'Appointment retrieval successfully',
+        meta: result.meta,
+        data: result.data,
+    });
+});
+
+
 export const appointmentController = {
-    createAppointment
+    createAppointment,
+    getMyAppointment,
+    getAllFromDB
 };
--- src/app/modules/Appointment/appointment.route.ts
+++ src/app/modules/Appointment/appointment.route.ts
@@ -2,15 +2,28 @@ import express from "express";
 import { appointmentController } from './appointment.controller';
 import auth from "../../middlewares/auth";
 import { UserRole } from "@prisma/client";
+import validateRequest from "../../middlewares/validateRequest";
+import { AppointmentValidation } from "./appointment.validation";
 
 const router = express.Router();
 
 router.post(
     "/",
     auth(UserRole.PATIENT),
-  
+    validateRequest(AppointmentValidation.createAppointment),
     appointmentController.createAppointment
   );
 
+  router.get(
+    '/my-appointment',
+    auth(UserRole.PATIENT, UserRole.DOCTOR),
+    appointmentController.getMyAppointment
+)
+
+router.get(
+  '/',
+  auth(UserRole.SUPER_ADMIN, UserRole.ADMIN),
+  appointmentController.getAllFromDB
+);
 
   export const AppointmentRoutes = router;
