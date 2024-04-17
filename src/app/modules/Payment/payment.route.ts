15f92a79b58bafaa9797c6f0dadd4320fd24de73
developerMasum <developer.masum2001@gmail.com>
2024-04-17 09:27:40 +0600

    added payment SSL

--- src/app/modules/Payment/payment.route.ts
+++ src/app/modules/Payment/payment.route.ts
@@ -4,6 +4,7 @@ import { paymentController } from "./payment.controller";
 const router = express.Router();
 
 router.post("/init-payment/:appointmentId", paymentController.initPayment);
+router.get("/ipn", paymentController.validatePayment);
 
 
 export const PaymentRoutes = router
--- src/app/modules/Payment/payment.service.ts
+++ src/app/modules/Payment/payment.service.ts
@@ -2,6 +2,7 @@ import axios from "axios";
 import config from "../../../config";
 import prisma from "../../../shared/prisma";
 import { SSLService } from "../SSL/ssl.service";
+import { PaymentStatus } from "@prisma/client";
 const initPayment = async (appointmentId: string) => {
   const paymentData = await prisma.payment.findFirstOrThrow({
     where: {
@@ -25,10 +26,57 @@ const initPayment = async (appointmentId: string) => {
   };
   const result = await SSLService.initPayment(initPaymentData);
   return {
-    paymentUrl: result.GatewayPageURL
+    paymentUrl: result.GatewayPageURL,
+  };
 };
-};
+
+const validatePayment = async (payload: any) => {
+  // if (!payload || !payload.status || !(payload.status === 'VALID')) {
+  //     return {
+  //         message: "Invalid Payment!"
+  //     }
+  // }
+
+  // const response = await SSLService.validatePayment(payload);
+
+  // if (response?.status !== 'VALID') {
+  //     return {
+  //         message: "Payment Failed!"
+  //     }
+  // }
+
+  const response = payload;
+  console.log(payload);
+
+  await prisma.$transaction(async (tx) => {
+      const updatedPaymentData = await tx.payment.update({
+          where: {
+              transactionId: response.tran_id
+          },
+          data: {
+              status: PaymentStatus.PAID,
+              paymentGatewayData: response
+          }
+      });
+
+      await tx.appointment.update({
+          where: {
+              id: updatedPaymentData.appointmentId
+          },
+          data: {
+              paymentStatus: PaymentStatus.PAID
+          }
+      })
+  });
+
+  return {
+      message: "Payment success!"
+  }
+
+}
+
 
 export const paymentService = {
   initPayment,
+  validatePayment,
 };
